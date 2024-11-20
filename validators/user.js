ce16cb79b8f5383fb8b90f0b4a05341326a79d0f
Charlotte Maanianu Seyab <charlotteseyab19@gmail.com>
2024-11-20 10:01:37 +0000

    fixed the user validator

--- validators/user.js
+++ validators/user.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const registerValidator = Joi.object({
-    username: Joi.string().min(3).max(30).required(),
+    name: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
     role: Joi.string().valid("admin", "user").default("user"),
