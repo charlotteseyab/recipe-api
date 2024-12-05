3058a67e5b31aecd7f66f2138342c2ee33d924b2
Charlotte Maanianu Seyab <charlotteseyab19@gmail.com>
2024-12-05 22:03:40 +0000

    updated upload.js in the middleware

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,25 +1,16 @@
 import multer from "multer";
 import { multerSaveFilesOrg } from "multer-savefilesorg";
 
-export const localUpload = multer ({
-    dest: 'uploads/',
-});
-
-// export const recipeIconUpload = multerSaveFilesOrg({
-//     storage: multerSaveFilesOrg({
-//         apiAccessToken: process.env.SAVEFILESORG_API_KEY,
-//         relativePath: '/recipe-api/recipe/*',
-//     }),
-//     preservePath:true
+// export const localUpload = multer({
+//     dest: 'uploads/'
 // });
 
+export const recipeIconUpload = multer({
+    storage: multerSaveFilesOrg({
+        apiAccessToken: process.env.SAVEFILESORG_API_KEY,
+        relativePath: '/recipe-api/recipe/*'
+    }),
+    preservePath: true
+})
 
-// Configure multerSaveFilesOrg storage
-const saveFilesOrgStorage = multerSaveFilesOrg({
-    apiAccessToken: process.env.SAVEFILESORG_API_KEY, // Your API key from .env
-    relativePath: '/recipe-api/recipe/*',             // Path for saved files
-    preservePath: true,                              // Optional: preserve directory structure
-});
 
-// Pass the storage to multer
-export const recipeIconUpload = multer({ storage: saveFilesOrgStorage });
