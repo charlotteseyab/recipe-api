d57ac7555b1e3a69c20f0f047a722929ec9d5863
Charlotte Maanianu Seyab <charlotteseyab19@gmail.com>
2024-12-06 09:31:11 +0000

    updated index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import { userRouter } from "./routes/user.js";
 import { recipeRouter } from "./routes/recipe.js";
 import { chefRouter } from "./routes/chef.js";
+import { commentRouter } from "./routes/comment.js";
 
 
 await mongoose.connect(process.env.MONGO_URI).then (() => console.log("Database connected successfully")).catch((error) => console.log("Error connecting to database", error));
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(userRouter);
 app.use(recipeRouter);
 app.use(chefRouter);
+app.use(commentRouter);
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`)
